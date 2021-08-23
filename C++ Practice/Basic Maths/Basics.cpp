04f3284613511b8a5456fe4fc313f7519387551d
Shidou <75203373+ShidouDairenji@users.noreply.github.com>
2021-08-23 17:46:40 +0530

    Create Basics.cpp

---
+++ C++ Practice/Basic Maths/Basics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+     std::cout << (123*321) << "\n" ; // Gives  Product
+     std::cout << (3221/123) << "\n" ; // Gives Quotient
+     std::cout << (321/123) << "\n" ; // Gives Remainder
+    
+     // Placing /n so that each output ends up in new line 
+     return 0;
+}
