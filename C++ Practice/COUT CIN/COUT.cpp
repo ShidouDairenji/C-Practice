b69537d7608504dd490966d835e3e517efbfb756
Shidou <75203373+ShidouDairenji@users.noreply.github.com>
2021-08-23 17:34:11 +0530

    Create COUT.cpp

---
+++ C++ Practice/COUT CIN/COUT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/* So much for one thing
+COUT covered */
+
+int main() 
+{
+     std::cout << "22222+22222 = ";
+     std::cout << (22222 + 22222);
+     std::cout << "\n";
+     std::cout << "Noob Problem" << "\n   Kek";
+     std::cout << " I pretty much used it in" << "\nEvery" << "\n Possible" ;
+     std::cout << "\n Way \n lol just flex";
+     return 0;
+}
