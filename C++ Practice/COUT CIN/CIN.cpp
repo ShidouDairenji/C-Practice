30067300ae347a4d75f76c43bdaaa2f0ff0221fd
Shidou <75203373+ShidouDairenji@users.noreply.github.com>
2021-08-24 19:11:52 +0530

    Create CIN.cpp

---
+++ C++ Practice/COUT CIN/CIN.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int num ; // int is the type of data
+std::cout << "Enter a number: " ;
+   std::cin >> num ; // cin will allow user to input value
+std::cout << "The number you entered is: " << num ; // haha noob
+  return 0;
+}
