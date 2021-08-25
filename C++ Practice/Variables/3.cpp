6e7ba890d1739bc928dccc46cc0cd223f2bc3285
Shidou <75203373+ShidouDairenji@users.noreply.github.com>
2021-08-25 15:43:10 +0530

    Update and rename 4.cpp to 3.cpp

---
+++ C++ Practice/Variables/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() 
+{
+  int var1 = 69,var2 = 88; // Assign values while declaring
+    std::cout << (var1 + var2); 
+  
+ int var3,var4; // Declaring
+    var3 = 65239 , var4 = 836; // Assigning
+    std::cout << "\n" << (var3 + var4);
+
+int var5 = 23333*var4+5353-72 ; // Small math calculation while assigning 
+int var6 = var1 + var2 + var3 + var4 ;
+std::cout << "\n" << (var6 +var5);
+  // Diffrent ways to work with vars
+  return 0;
+}
